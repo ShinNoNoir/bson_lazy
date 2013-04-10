e555db5e4d9a4f7cb5b65f749219cebec9dbf512
WoLpH <Rick@Fawo.nl>
2013-04-10 17:04:06 +0200

    switched to setuptools to enable sphinx build and upload

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-
+import os
 import sys
+
 if sys.version < '2.6':
     print 'Python >= 2.6 required'
     sys.exit(1)
 
-from distutils.core import setup
+from setuptools import setup
+
+if os.path.isfile('README.rst'):
+    long_description = open('README.rst').read()
+else:
+    long_description = '''A lazy BSON file loader that leverages PyMongo's
+    built-in BSON decoder. Comes with a bson2json utility.'''
 
 setup(
     name = 'bson_lazy',
@@ -18,14 +25,13 @@ setup(
     scripts=['bin/bson2json.py'],
     
     description = 'A lazy BSON file loader',
-    long_description = \
-    """A lazy BSON file loader that leverages PyMongo's built-in BSON decoder. """ + \
-    """Comes with a bson2json utility.""",
+    long_description = long_description,
     platforms = 'Any',
     license = 'LICENSE.txt',
     keywords = 'BSON lazy',
     
     install_requires = [
-        'PyMongo >= 2.5'
+        'PyMongo >= 2.5',
     ],
 )
+
